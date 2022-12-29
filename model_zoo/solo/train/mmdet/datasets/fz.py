b171b1be0b05a97f44f7a664b1b6d5a8cb0f0926
stj <stj@keba.com>
2022-12-29 15:59:38 +0100

    add fz dataset

---
+++ model_zoo/solo/train/mmdet/datasets/fz.py
@@ -0,0 +1,8 @@
+from .coco import CocoDataset
+from .registry import DATASETS
+
+
+@DATASETS.register_module
+class FzDataset(CocoDataset):
+
+    CLASSES = ('blauer_Ball', 'gelber_Ball', 'roter_Ball', 'background')
