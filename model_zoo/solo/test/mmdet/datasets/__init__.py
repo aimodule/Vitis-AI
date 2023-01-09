0305c3ab70a1050b192fcb856df0d266892e323a
stj <stj@keba.com>
2023-01-09 11:36:30 +0100

    fz dataset

--- model_zoo/solo/test/mmdet/datasets/__init__.py
+++ model_zoo/solo/test/mmdet/datasets/__init__.py
@@ -9,10 +9,11 @@ from .voc import VOCDataset
 from .wider_face import WIDERFaceDataset
 from .xml_style import XMLDataset
 from .table import TableDataset
+from .fz import FzDataset
 
 __all__ = [
     'CustomDataset', 'XMLDataset', 'CocoDataset', 'VOCDataset',
     'CityscapesDataset', 'GroupSampler', 'DistributedGroupSampler',
     'build_dataloader', 'ConcatDataset', 'RepeatDataset', 'WIDERFaceDataset',
-    'DATASETS', 'build_dataset', 'TableDataset'
+    'DATASETS', 'build_dataset', 'TableDataset', 'FzDataset'
 ]
